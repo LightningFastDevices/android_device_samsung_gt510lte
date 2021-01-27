f0924489851dea810de6bd7ffcedce8acbdd9c5e
zeeshan <itexpert120@gmail.com>
2021-01-27 20:22:13 +0500

    new lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 LOCAL_PATH := device/samsung/gt510lte
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_gt510lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gt510lte-eng \
+    lineage_gt510lte-userdebug \
+    lineage_gt510lte-user
